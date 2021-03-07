af74f3cd640f4b2bd8866696ea16b61b9edd6418
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-03-07 16:23:17 -0500

    about page making

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import Girl from '../pexels-andrea-piacquadio-733872.jpg';
 
 const About = () => {
     return (
-        <div className="about">
-            
+      <div className="container">
+        <div className="about mt-xl">
+          <div className="about__content">
+            <div className="heading h-3 h-3--black">About Me</div>
+            <div className="about__details">
+              <div className="h-4 mt-s mb-s">Suzanne Diloski</div>
+              <div className="h-4 mt-s mb-s">Sales Representative</div>
+              <div className="h-4 mt-s mb-s">Suzanne Diloski</div>
+            </div>
+          </div>
+          <div className="about__image">
+            <img src={Girl} alt="" />
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 export default About
