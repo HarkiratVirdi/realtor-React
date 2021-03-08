4d7c200dd8c3533da67719f69e63702be76d47cd
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-03-07 23:04:49 -0500

    links made

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,29 +3,35 @@ import FormField from './FormField'
 
 const Footer = () => {
     return (
-        <div className="footer__background" style={{background: '#252526'}}>
-
-    <div className="container"> 
-        <div className="footer">
+      <div className="footer__background" style={{ background: "#252526" }}>
+        <div className="container">
+          <div className="footer">
             <div className="h-3 h-3--cta mt-l">Free Real Estate Reports</div>
             <div className="footer__email">
-            <FormField inputType="text" labelFor="Name"/>
-            <FormField inputType="email" labelFor="Email"/>
+              <FormField inputType="text" labelFor="Name" />
+              <FormField inputType="email" labelFor="Email" />
             </div>
-            <button className="border-round cta-link btn-contact btn-cta mt-s">Get Free Reports</button>
+            <button className="border-round cta-link btn-contact btn-cta mt-s">
+              Get Free Reports
+            </button>
 
             <ul className="footer__links">
-                <li className="h-4 h-3--white">+1 6476851425 </li>
-                <li className="h-4 h-3--white">suzannediloski@gmail.com </li>
-                <li className="h-4 h-3--white">Instagram </li>
-                <li className="h-4 h-3--white">Facebook </li>
-                <li className="h-4 h-3--white">Realtor </li>
+              <li className="h-4 h-3--white">
+                <a className="h-4 h-4--white" href="tel:647-685-1425">647-685-1425 </a>{" "}
+              </li>
+              <li className="h-4 h-3--white">
+                <a className="h-4 h-4--white" href="mailTo:suzannediloski@gmail.com ">
+                  suzannediloski@gmail.com{" "}
+                </a>{" "}
+              </li>
+              <li className="h-4 h-3--white"><a className="h-4 h-4--white" href="#">Instagram</a> </li>
+              <li className="h-4 h-3--white"><a className="h-4 h-4--white" href="#">Facebook</a> </li>
+              <li className="h-4 h-3--white"><a className="h-4 h-4--white" href="#">Realtor</a> </li>
             </ul>
-
-            </div>
+          </div>
         </div>
-        </div>
-    )
+      </div>
+    );
 }
 
 export default Footer
