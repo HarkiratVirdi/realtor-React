7899d84ab57fc03326e9c58604efd7fbe1eb63dc
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-03-07 22:57:46 -0500

    footer made

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,27 @@ import FormField from './FormField'
 
 const Footer = () => {
     return (
-    <div className="container" style={{background: '#252526'}}> 
+        <div className="footer__background" style={{background: '#252526'}}>
+
+    <div className="container"> 
         <div className="footer">
             <div className="h-3 h-3--cta mt-l">Free Real Estate Reports</div>
             <div className="footer__email">
             <FormField inputType="text" labelFor="Name"/>
             <FormField inputType="email" labelFor="Email"/>
             </div>
-            <button className="border-round cta-link btn-contact btn-cta">Get Free Reports</button>
+            <button className="border-round cta-link btn-contact btn-cta mt-s">Get Free Reports</button>
+
+            <ul className="footer__links">
+                <li className="h-4 h-3--white">+1 6476851425 </li>
+                <li className="h-4 h-3--white">suzannediloski@gmail.com </li>
+                <li className="h-4 h-3--white">Instagram </li>
+                <li className="h-4 h-3--white">Facebook </li>
+                <li className="h-4 h-3--white">Realtor </li>
+            </ul>
+
             </div>
+        </div>
         </div>
     )
 }
--- src/components/FormField.js
+++ src/components/FormField.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FormField = ({labelFor, inputType}) => {
     return (
-        <div className="formfield h-4 mt-l mb-l">
+        <div className="formfield h-4 mt-s mb-s">
             <label htmlFor={labelFor}>{labelFor}</label>
             <input className="" type={inputType} />
         </div>
